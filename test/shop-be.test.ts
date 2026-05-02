cca0d4f8527b438028f82430529431adc045df61
meta-panic <ritaritarita2013@gmail.com>
2026-05-02 13:12:03 +0400

    feat: add lambdas

---
+++ test/shop-be.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShopBe from '../lib/shop-be-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shop-be-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new ShopBe.ShopBeStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
